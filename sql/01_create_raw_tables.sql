75a3b7f592f22eb422d537f316819459aeb911ee
MAYANK P. SAVANI <127303160+MYANK-11@users.noreply.github.com>
2025-12-25 13:22:33 +0530

    Delete sql directory

--- sql/01_create_raw_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE raw_events (
-    event_id        BIGINT PRIMARY KEY,
-    user_id         BIGINT NOT NULL,
-    event_name      TEXT   NOT NULL,
-    event_timestamp TIMESTAMP NOT NULL,
-    device_type     TEXT   NOT NULL,
-    country         TEXT   NOT NULL,
-    plan_type       TEXT
-);
